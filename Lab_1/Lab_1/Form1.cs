a7ebec9b8e91a9fd46b91e4eb4679add9b3351a0
Kozyava <48145809+25081998@users.noreply.github.com>
2021-02-18 16:10:23 +0300

    Add files via upload

---
+++ Lab_1/Lab_1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CGLabPlatform;
+
+namespace Lab_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var gdiDevice = new GDIDevice();
+            gdiDevice.Dock = DockStyle.Fill;
+            gdiDevice.BackColor = Color.LightSteelBlue;
+            this.panel1.Controls.Add(gdiDevice);
+            gdiDevice.DeviceUpdate += GdiDevice_DeviceUpdate;
+
+        }
+
+        private void GdiDevice_DeviceUpdate(object sender, GDIDeviceUpdateArgs e)
+        {
+            e.Surface.DrawLine(9, 0, 0, 200, 300);
+        }
+    }
+}
